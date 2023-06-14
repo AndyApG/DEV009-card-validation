3238e2a2fdf48c4436a8716daed06e47ae94024d
Andrea <a.aparicio.g@hotmail.com>
2023-06-14 16:33:50 -0600

    added alert and confirm  messages, created listOfCards object to save data

--- src/index.js
+++ src/index.js
@@ -1,47 +1,23 @@
 import validator from './validator.js';
 
-function saveCards(){
-
-    const tipo=document.getElementsByName("tipo").values();
-    const name= document.getElementById("name").value;
-    const creditCardNumber = document.getElementById("number").value;
-    const valid=document.getElementById("valid").value;
-    const cvv= document.getElementById("cvv").value;
-
-    if (validator.isValid(creditCardNumber) & creditCardNumber != null){
-
-        const cardElement = {
-            type: tipo,
-            titular: name,
-            cardNumber: creditCardNumber,
-            fecha: valid,
-            cvv: cvv       
-        };
-        alert("Tarjeta Guardada")
-        
-
-    }else alert("La tarjeta con el numero "+validator.maskify(creditCardNumber)+" es invalida.");
-    
-}
-
 let nameWriting= document.querySelector('#name');
 let nameWrited=document.getElementById('name1');
 
 nameWriting.addEventListener('keyup',()=>{
-    nameWrited.innerHTML=nameWriting.value;
+    nameWrited.textContent=nameWriting.value;
 });
 
 let tipoSelected1=document.getElementById('credito');
-let tipoWrite=document.getElementById('tipo1');
+let tipoWrited=document.getElementById('tipo1');
 
 tipoSelected1.addEventListener('click',()=>{
-    tipoWrite.innerHTML=tipoSelected1.value;
+    tipoWrited.textContent=tipoSelected1.value;
 });
 
 let tipoSelected2=document.getElementById('debito');
 
 tipoSelected2.addEventListener('click',()=>{
-    tipoWrite.innerHTML=tipoSelected2.value;
+    tipoWrited.textContent=tipoSelected2.value;
 });
 
 let numberWriting= document.querySelector('#number');
@@ -49,14 +25,14 @@ let numberWrited=document.getElementById('number1');
 
 numberWriting.addEventListener('keyup',()=>{
     
-    numberWrited.innerHTML=validator.maskify(numberWriting.value);
+    numberWrited.textContent=validator.maskify(numberWriting.value);
 });
 
 let cvvWriting= document.querySelector('#cvv');
 let cvvWrited=document.getElementById('cvv1');
 
 cvvWriting.addEventListener('keyup',()=>{
-    cvvWrited.innerHTML=cvvWriting.value;
+    cvvWrited.textContent=cvvWriting.value;
 
 });
 
@@ -64,6 +40,26 @@ let validWriting= document.getElementById('valid');
 let validWrited=document.getElementById('mes');
 
 validWriting.addEventListener('input',()=>{
-    validWrited.innerHTML=validWriting.value;
+    validWrited.textContent=validWriting.value;
 });
 
+document.getElementById('validar').addEventListener('click',(numberWrited) =>{
+    if(numberWrited.value !=='0000000000000000' && numberWriting.value !== ''){
+        const creditCardNumber = document.getElementById("number").value;
+        if(validator.isValid(creditCardNumber)){
+            if(confirm("El número de tarjeta es valido.\n ¿Deseas Guardarlo?")){
+                let tipo =document.getElementById('tipo1').value;
+                let listOfCards = {
+                    type : tipoWrited.value,
+                    name : nameWrited.value,
+                    number : creditCardNumber,
+                    cvv : cvvWrited.value,
+                    date: validWrited.value,
+                };
+                alert('tipo de tarjeta: '+ listOfCards.type+'\n titular:'+ listOfCards.name+'\n numero de tarjeta: '+
+                listOfCards.number+'\n cvv:'+listOfCards.cvv+'\n valida hasta: '+listOfCards.date);
+            }
+            
+        }else alert("Número de tarjeta invalido");
+    }else alert("No hay datos que guardar");
+});
